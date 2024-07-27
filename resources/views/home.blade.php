10f7b9872d20e4deac541bd2e2466d4bd33b8b32
NipunLiyanage69 <nipun.rightmo@gmail.com>
2024-07-27 17:03:18 +0530

    switching plans now possible

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,18 +11,23 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-    <!-- Styles -->
     @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+
+    <script src="https://www.paypal.com/sdk/js?client-id={{ env('PAYPAL_CLIENT_ID') }}&vault=true&intent=subscription">
+    </script>
 </head>
 
 <body class="font-sans antialiased">
 
     <div class="container mx-auto px-4 py-8 text-center">
         <h2 class="text-4xl font-semibold text-black text-center">Plans</h2>
-        <div class="flex flex-wrap md:flex-nowrap flex-col md:flex-row justify-center content-center gap-6 mt-8">
+        <div class="flex flex-wrap md:flex-nowrap flex-col md:flex-row justify-center content-center gap-6 mt-8"
+            x-data="{ selectedPlan: '{{ $plans->first()->id }}' }">
             @foreach ($plans as $plan)
-                <div
-                    class="w-80 p-4 bg-[#FF2D20]/10 rounded-lg shadow-lg px-6 py-8 text-center cursor-pointer hover:bg-[#FF2D20]/20 transition duration-300 ">
+                <div class="w-80 p-4 bg-[#FF2D20]/10 rounded-lg shadow-lg px-6 py-8 text-center cursor-pointer hover:bg-[#FF2D20]/20 transition duration-300"
+                    x-on:click="selectedPlan = '{{ $plan->id }}'; console.log(selectedPlan)"
+                    x-bind:class="{ 'bg-[#FF2D20]/20 border border-[#FF2D20]/50': selectedPlan === '{{ $plan->id }}' }">
                     <h3 class="text-xl font-bold text-[#FF2D20] mb-4 uppercase tracking-widest">{{ $plan->name }}</h3>
                     <p class="text-gray-500 mb-4 text-sm">{{ $plan->description }}</p>
                     <p class="text-[#FF2D20] mb-4 font-bold uppercase tracking-widest text-3xl">
@@ -30,7 +35,32 @@
                 </div>
             @endforeach
         </div>
+        <div class="mt-8 mx-auto w-80" id="paypal-button-container"></div>
     </div>
+
+
+    <script></script>
+
+
+    <script>
+        paypal.Buttons({
+            style: {
+                shape: 'rect',
+                color: 'gold',
+                layout: 'horizontal',
+                label: 'paypal'
+            },
+            createSubscription: function(data, actions) {
+                return actions.subscription.create({
+                    /* Creates the subscription */
+                    plan_id: 'P-7DH95831107882341M2RA4TI'
+                });
+            },
+            onApprove: function(data, actions) {
+                alert(data.subscriptionID); // You can add optional success message for the subscriber here
+            }
+        }).render('#paypal-button-container'); // Renders the PayPal button
+    </script>
 
 </body>
 
